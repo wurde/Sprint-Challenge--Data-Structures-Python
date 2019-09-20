04f2194c8f5daf150e10a06eb244b12678f6d6ac
Andy Bettisworth <andy@accreu.com>
2019-09-20 10:45:13 -0500

    Encapsulate logic in method and add comments

--- names/names.py
+++ names/names.py
@@ -1,22 +1,39 @@
+#
+# Dependencies
+#
+
 import time
 
-start_time = time.time()
+#
+# Define method
+#
 
-f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
-f.close()
+def checkDuplicates():
+    start_time = time.time()
 
-f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
-f.close()
+    f = open('names_1.txt', 'r')
+    names_1 = f.read().split("\n")  # List containing 10000 names
+    f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    f = open('names_2.txt', 'r')
+    names_2 = f.read().split("\n")  # List containing 10000 names
+    f.close()
 
-end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+    duplicates = []
+    for name_1 in names_1:
+        for name_2 in names_2:
+            if name_1 == name_2:
+                duplicates.append(name_1)
 
+    end_time = time.time()
+    print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+    #=> 64 duplicates: ...
+    print (f"runtime: {end_time - start_time} seconds")
+    #=> runtime: 1.871626377105713 seconds
+
+#
+# Run method
+#
+
+if __name__ == '__main__':
+    checkDuplicates()
