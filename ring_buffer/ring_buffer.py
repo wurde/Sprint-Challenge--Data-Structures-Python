4f7f1cc68b955feabedabceb0d0d7766473916e3
Andy Bettisworth <andy@accreu.com>
2019-09-20 10:19:50 -0500

    Add #append and #get to RingBuffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,10 +6,14 @@ class RingBuffer:
   def __init__(self, capacity):
     self.capacity = capacity
     self.current = 0
-    self.storage = [None]*capacity
+    self.storage = [None] * capacity
 
   def append(self, item):
-    pass
+      self.storage[self.current] = item
+      self.current += 1
+
+      if self.current >= self.capacity:
+          self.current = 0
 
   def get(self):
-    pass
+      return [x for x in self.storage if x is not None]
