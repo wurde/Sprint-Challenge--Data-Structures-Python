089a1f00a14d693b9e901952d6c953aa90eb5889
Andy Bettisworth <andy@accreu.com>
2019-09-20 10:47:58 -0500

    Cut runtime in half

--- names/names.py
+++ names/names.py
@@ -21,15 +21,21 @@ def checkDuplicates():
 
     duplicates = []
     for name_1 in names_1:
-        for name_2 in names_2:
-            if name_1 == name_2:
-                duplicates.append(name_1)
+        if name_1 in names_2:
+            duplicates.append(name_1)
+
+    # ORIGINAL code
+    # duplicates = []
+    # for name_1 in names_1:
+    #     for name_2 in names_2:
+    #         if name_1 == name_2:
+    #             duplicates.append(name_1)
 
     end_time = time.time()
     print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
     #=> 64 duplicates: ...
     print (f"runtime: {end_time - start_time} seconds")
-    #=> runtime: 1.871626377105713 seconds
+    #=> runtime: 0.7826683521270752 seconds
 
 #
 # Run method
